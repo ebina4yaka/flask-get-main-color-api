470bf6de118ea2bcfab645cdf53f74c7050e93e0
ebina4yaka <4yaka3bina@gmail.com>
2020-12-03 12:42:45 +0900

    Add route

--- src/app.py
+++ src/app.py
@@ -21,5 +21,11 @@ def upload():
     return jsonify({"colors": get_main_colors(img_stream)})
 
 
+@app.route('/api/upload/clusters/<clusters>', methods=['POST'])
+def upload_with_clusters(clusters):
+    img_stream: bytes = base64.b64decode(request.json['image'])
+    return jsonify({"colors": get_main_colors(img_stream, clusters)})
+
+
 if __name__ == '__main__':
     app.run(debug=False, threaded=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
